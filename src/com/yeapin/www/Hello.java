69a6f10706667c50922272253b98e7ead2ecc6bc
jiangjunjie <junjie5678@hotmail.com>
2016-02-29 23:33:29 +0800

    hello

---
+++ src/com/yeapin/www/Hello.java
@@ -0,0 +1,13 @@
+package com.yeapin.www;
+
+/**
+ * Hello
+ * 
+ * @author jiangjunjie
+ *
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
